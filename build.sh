0f27078fca289c105a8e8a8207031595655aa413
waffler12345 <gabriel.petesch@gmail.com>
2022-10-19 22:46:18 -0400

    Static Site Generator for Bash

---
+++ build.sh
@@ -0,0 +1,3 @@
+cat templates/top.html content/index.html templates/bottom.html > docs/index.html
+cat templates/top.html content/events.html templates/bottom.html > docs/events.html
+cat templates/top.html content/about.html templates/bottom.html > docs/about.html
