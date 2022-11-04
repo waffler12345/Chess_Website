d03ca1ea430c709aef6745b810c14696129fc0f1
waffler12345 <gabriel.petesch@gmail.com>
2022-11-04 01:16:20 -0400

    added main() to build.py and removed build.sh

--- build.py
+++ build.py
@@ -1,20 +1,24 @@
-#templates-bottom and top of each page
-top=open("templates/top.html").read()
-bottom=open("templates/bottom.html").read()
+def main():
 
-#content
-index=open("content/index.html").read()
-events=open("content/events.html").read()
-about=open("content/about.html").read()
+    #templates-bottom and top of each page
+    top=open("templates/top.html").read()
+    bottom=open("templates/bottom.html").read()
 
-#combine into one variable
-index_page= top + index + bottom
-events_page= top + events + bottom
-about_page= top + about + bottom
+    #content
+    index=open("content/index.html").read()
+    events=open("content/events.html").read()
+    about=open("content/about.html").read()
 
-#write result into a new file in docs directory
-open("docs/index.html", "w+").write(index_page)
-open("docs/events.html", "w+").write(events_page)
-open("docs/about.html", "w+").write(about_page)
+    #combine into one variable
+    index_page= top + index + bottom
+    events_page= top + events + bottom
+    about_page= top + about + bottom
+
+    #write result into a new file in docs directory
+    open("docs/index.html", "w+").write(index_page)
+    open("docs/events.html", "w+").write(events_page)
+    open("docs/about.html", "w+").write(about_page)
+
+main()
 
 
